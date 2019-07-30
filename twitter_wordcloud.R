952e693062a045002fc21f5cbe9d47e04627d964
Kwangho.Woo <wookh1@gmail.com>
2019-07-30 23:34:44 +0900

    minor changes

--- twitter_wordcloud.R
+++ twitter_wordcloud.R
@@ -1,5 +1,4 @@
-# Making wordcloud with R #
-
+# twitteR scrip #
 rm(list = ls())
 if (!requireNamespace("base64enc"))
   install.packages("base64enc")
@@ -13,34 +12,27 @@ library("base64enc")
 library("twitteR")
 library("ROAuth")
 library("RCurl")
-library("KoNLP")
-library("tm")
 
 reqURL <- "http://api.twitter.com/oauth/request_token"
 accessURL <- "http://api.twitter.com/oauth/access_token"
 authURP <- "http://api.twitter.com/oauth/authorize"
 
-consumerKey <- 
-  "0hj2HIxjNZfVh5KxU84Sm6cp8"
+consumerKey <- "0hj2HIxjNZfVh5KxU84Sm6cp8"
 consumerSecert <-
   "AolyqCnyMm8pXCaGpBXVzxW6cY77OxQWi23ym4QxAGvT3ttPdF"
-accesstoken <- 
-  "518299811-pfAvgAV2tWGmQ1eEZyAcZ1SScS0DlRDXVUTQimFl"
-accesstokensecret <- 
-  "r7hri2qlI3evKL5LS59k7zVTDxvbtBAV9sNFwLxg2iItV"
+accesstoken <- "518299811-pfAvgAV2tWGmQ1eEZyAcZ1SScS0DlRDXVUTQimFl"
+accesstokensecret <- "r7hri2qlI3evKL5LS59k7zVTDxvbtBAV9sNFwLxg2iItV"
 
 options(RCurlOptions = list(cainfo = system.file("CurlssL", "cacert.pem", package = "RCurl")))
 download.file(url = "http://curl.haxx.se/ca/cacert.pem", destfile = "cacert.pem")
+
 setup_twitter_oauth(consumerKey, consumerSecert, accesstoken, accesstokensecret)
 
 n <- 2000
 keyword <- '근로시간'
-#keyword <- enc2utf8(keyword)
-# option; language, date, altitude & latitude 
-tweets.wh<-searchTwitter(keyword,n=2000, lang="ko", 
-                         since ="2018-7-01", 
-                         until="2019-06-20", 
-                         geocode='37.566535,126.977969, 500km')
+keyword <- enc2utf8(keyword)
+
+tweets.wh <- searchTwitter(keyword, n)
 
 is.data.frame(tweets.wh) #데이터 프레임 여부 체크 
 tweets.wh.df<-twListToDF(tweets.wh) #트윗 리스트 - 데이터 프레임 변환
@@ -79,11 +71,7 @@ head(txt.word,5)
 # 워드 클라우드 사용하지 않은 단어 제거
 tweets.word<- gsub("[[:punct:]]","", tweets.word)
 tweets.word<- gsub("[0-9]+[A-Za-z]", "", tweets.word) #숫자+알파벳 제거 
-tweets.word<- gsub("남북회담", "", tweets.word) #빈도 분석 후 불필요 단어 제거 
-tweets.word<- gsub("문재", "", tweets.word)
-tweets.word<- gsub("남북", "", tweets.word)
-tweets.word<- gsub("회담", "", tweets.word)
-tweets.word<- gsub("co", "", tweets.word)
+tweets.word<- gsub("근로시간", "", tweets.word)
 #단어 길이 2개 이상 선택
 tweets.word<- Filter(function(x){nchar(x)>=2}, tweets.word)
 
@@ -95,5 +83,17 @@ head(sort(tweets.count, decreasing=T), 10)
 library(RColorBrewer)
 library(wordcloud)
 pal <- brewer.pal(7,"Paired") # 컬러 세팅 
-wordcloud(names(tweets.count),freq=tweets.count,scale=c(4,0.5),min.freq=5,random.order=F,rot.per=.1,colors=pal,family='AppleGothic')
+wordcloud(names(tweets.count),freq=tweets.count,min.freq=5,random.order=F,rot.per=.1,colors=pal,family='AppleGothic')
+
+
+wordcloud(
+  names(tweets.count),
+  freq=tweets.count,
+  scale=c(4,0.2), #빈도가 가장 큰 단어와 가장 빈도가 작은단어 폰사 사이 크기
+  rot.per=0.1, #90도 회전해서 보여줄 단어 비율
+  min.freq=3, max.words=100, # 빈도 3이상, 100미만
+  random.order=F, # True : 랜덤배치, False : 빈도수가 큰단어를 중앙에 배치
+  random.color=T, # True : 색랜덤, False : 빈도순
+  colors=brewer.pal(11, "Paired"), #11은 사용할 색상개수, 두번째는 색상타입이름
+  family="AppleGothic")
 
